3d73f457fe3802500386649533e73deb25e8ce79
Kaustav Kumar <bitkaustav@gmail.com>
2026-07-12 20:00:07 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (82.05%) - LeetSync

---
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    void dfs(vector<vector<char>>& board, int row, int col){
+        if(row < 0 || row >= board.size() || col < 0 || col >= board[0].size()){
+            return;
+        }
+        if(board[row][col] != 'O'){
+            return;
+        }
+        board[row][col] = '#';
+        dfs(board, row-1, col);
+        dfs(board, row+1, col);
+        dfs(board, row, col+1);
+        dfs(board, row, col-1);
+    }
+    void solve(vector<vector<char>>& board) {
+        int rows = board.size();
+        int cols = board[0].size();
+
+        for(int i=0; i<rows; i++){
+            if(board[i][0] == 'O'){
+                dfs(board, i, 0);
+            }
+        }
+        for(int i=0; i<rows; i++){
+            if(board[i][cols-1] == 'O'){
+                dfs(board, i, cols-1);
+            }
+        }
+        for(int j=0; j<cols; j++){
+            if(board[0][j] == 'O'){
+                dfs(board, 0, j);
+            }
+        }
+        for(int j=0; j<cols; j++){
+            if(board[rows-1][j] == 'O'){
+                dfs(board, rows-1, j);
+            }
+        }
+        for(int i = 0; i < rows; i++){
+
+            for(int j = 0; j < cols; j++){
+
+                if(board[i][j] == 'O')
+                    board[i][j] = 'X';
+
+                else if(board[i][j] == '#')
+                    board[i][j] = 'O';
+            }
+        }
+        
+    }
+};
