c0f164e8d8bac83f963d134d8c1e50071593fdd7
Kaustav Kumar <bitkaustav@gmail.com>
2026-08-01 14:09:09 +0530

    Time: 37 ms (96.66%) | Memory: 29 MB (92.75%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,31 +1,35 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
         vector<vector<int>> ans;
-        int n = nums.size();
-        for (int i = 0; i < n - 1; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) {
+        sort(nums.begin(), nums.end());
+        for(int i=0; i<nums.size()-2; i++){
+            if(nums[i] > 0){
+                break;
+            }
+            if(i > 0  && nums[i] == nums[i-1]){
                 continue;
             }
-            int left = i + 1;
-            int right = n - 1;
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-                if (sum == 0) {
-                    ans.push_back({nums[i], nums[left], nums[right]});
-                    left++;
+            int left = i+1;
+            int right = nums.size()-1;
+            while(left < right){
+                int sum = nums[left] + nums[right] + nums[i];
+                if(sum > 0){
                     right--;
-                    while (left < right && nums[left] == nums[left - 1]) {
+                }
+                else if(sum < 0) {
+                    left++;
+                }
+                else{
+                    ans.push_back({nums[left], nums[right], nums[i]});
+                    right--;
+                    left++;
+                    while(left < right && nums[left] == nums[left-1]){
                         left++;
                     }
-                    while (left < right && nums[right] == nums[right + 1]) {
+                    while(left < right && nums[right] == nums[right+1]){
                         right--;
                     }
-                } else if (sum < 0) {
-                    left++;
-                } else {
-                    right--;
                 }
             }
         }
